5a46097a5ddec73b15499390c0b43739cd6212fc
younghwan90s <younghwan1608@gmail.com>
2022-12-06 23:15:30 +0900

    마이페이지, 회원탈퇴 기능 구현 업데이트

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ app.secret_key = 'my secret key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '비밀번호를 여기 입력하세용 :)'
+app.config['MYSQL_PASSWORD'] = 'sksms9604'
 app.config['MYSQL_DB'] = 'talmo'
+app.config['MYSQL_CHARSET']= 'utf8'
 
 # MySQL 실행
 mysql = MySQL(app)
@@ -42,7 +43,7 @@ def login():
             return redirect(url_for('index'))
         else:
             msg = '잘못된 아이디/비밀번호 입니다!'
-    return render_template('signIn.html', msg=msg)
+    return render_template('login.html', msg=msg)
 
 # 로그아웃
 @app.route('/logout')
@@ -53,21 +54,43 @@ def logout():
    session.pop('name', None)
    return redirect(url_for('login'))
 
-
+# 로그인 됬을 때 메인페이지로 이동 id=session['id']
 @app.route('/index',)
 def index():
     if 'loggedin' in session:
-        return render_template('index.html', name=session['name'])
+        return render_template('index.html', id=session['id'])
     return redirect(url_for('login'))
 
+# 홈 눌렀을 때 메인페이지 이동
 @app.route('/')
 def home():
    return render_template('index.html')
 
+# 마이페이지
+@app.route('/mypage')
+def mypage():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM account WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        return render_template('myPage.html', account=account)
+    return redirect(url_for('login'))
+
+# 회원탈퇴
+@app.route('/removeUser')
+def removeUser():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('DELETE FROM account WHERE id = %s', (session['id'],))
+        mysql.connection.commit()
+        mysql.connection.close()
+    return redirect(url_for('login'))
+
+
 # 피드 불러오기
 @app.route('/feed', methods=["GET"])
 def getFeedDB():
-    db = mysql.connect
+    db = pymysql.connect(host='localhost', user='root', db='talmo', password='sksms9604', charset='utf8')
     curs = db.cursor()
 
     # feed 테이블에서 feedId, feedDate 불러오기
@@ -99,17 +122,18 @@ def getFeedDB():
 
 
 # 피드 댓글을 DB에 등록하기
-@app.route("/feed/post", methods=["POST"])
+@app.route('/feed/post', methods=["POST"])
 def saveCommentDB():
-    db = mysql.connect
+    db = pymysql.connect(host='localhost', user='root', db='talmo', password='sksms9604', charset='utf8')
     curs = db.cursor()
 
     feedComment = request.form['comment_give']
     feedDate = request.form['date_give']
+    uniqueId = session['uniqueId']
 
     # feed 테이블로 등록
-    sql = '''insert into feed (feedComment, feedDate) values(%s,%s)'''
-    curs.execute(sql, (feedComment, feedDate))
+    sql = '''insert into feed (feedComment, feedDate, uniqueId) values(%s,%s,%s)'''
+    curs.execute(sql, (feedComment, feedDate, uniqueId))
 
     db.commit()
     db.close()
@@ -117,13 +141,12 @@ def saveCommentDB():
 
 
 # 피드 댓글을 DB에 삭제하기
-@app.route("/feed", methods=["delete"])
+@app.route('/feed', methods=["delete"])
 def deleteCommentDB():
-    db = mysql.connect
+    db = pymysql.connect(host='localhost', user='root', db='talmo', password='sksms9604', charset='utf8')
     curs = db.cursor()
 
     feedId = request.form['feedId_give']
-    print(feedId)
 
     # feed 테이블에 feedId에 해당하는 댓글 삭제
     sql = '''DELETE FROM talmo.feed WHERE feedId = %s'''
@@ -132,7 +155,6 @@ def deleteCommentDB():
     db.commit()
     db.close()
     return jsonify({'msg': '삭제 완료!'})
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
