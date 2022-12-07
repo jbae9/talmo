4e3dc318fb6ee752ee28c13f3e462a14297812ec
jeonggi <wjdrl3122@gmail.com>
2022-12-07 15:11:36 +0900

    커밋 입니다.

--- app.py
+++ app.py
@@ -5,18 +5,21 @@ import pymysql
 import json
 
 import certifi
+
 ca = certifi.where()
 
 app = Flask(__name__)
 
 app.secret_key = 'my secret key'
 
-connection = mysql.connector.connect(host='localhost', user='root', db='talmo', password='sksms9604')
+connection = mysql.connector.connect(host='localhost', user='root', db='talmo', password='wjdrl')
 cursor = connection.cursor()
 
+
 def getDB():
-    db = pymysql.connect(host='localhost', user='root', db='talmo', password='password', charset='utf8')
+    db = pymysql.connect(host='localhost', user='root', db='talmo', password='wjdrl', charset='utf8')
     return db
+
 
 # 로그인
 @app.route('/', methods=['GET', 'POST'])
@@ -28,7 +31,7 @@ def login():
 
         cursor.execute('SELECT * FROM account WHERE id = %s AND pw = %s', (id, pw))
         account = cursor.fetchone()
-    
+
         if account:
             session['loggedin'] = True
             session['uniqueId'] = account[0]
@@ -39,26 +42,30 @@ def login():
             msg = '잘못된 아이디/비밀번호 입니다!'
     return render_template('login.html', msg=msg)
 
+
 # 로그아웃
 @app.route('/logout')
 def logout():
-   session.pop('loggedin', None)
-   session.pop('uniqueId', None)
-   session.pop('id', None)
-   session.pop('name', None)
-   return redirect(url_for('login'))
+    session.pop('loggedin', None)
+    session.pop('uniqueId', None)
+    session.pop('id', None)
+    session.pop('name', None)
+    return redirect(url_for('login'))
+
 
 # 로그인 됬을 때 메인페이지로 이동 id=session['id']
-@app.route('/index',)
+@app.route('/index')
 def index():
     if 'loggedin' in session:
         return render_template('index.html', id=session['id'])
     return redirect(url_for('login'))
 
+
 # 홈 눌렀을 때 메인페이지 이동
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 # 마이페이지
 @app.route('/mypage')
@@ -68,6 +75,7 @@ def mypage():
         account = cursor.fetchone()
         return render_template('myPage.html', account=account)
     return redirect(url_for('login'))
+
 
 # 회원탈퇴
 @app.route('/removeUser')
@@ -89,7 +97,7 @@ def getFeedDB():
     # account 테이블이랑 uniqueId로 LEFT JOIN해서 name 불러오기
     # 최신순으로 등록된 데이터을 받음
     sql = """
-    SELECT 	feedId,
+    SELECT feedId,
 		date_format(`feedDate`, '%Y-%c-%d %h:%i %p') as feedDate,
 		feedComment,
 		a.name,
@@ -109,6 +117,7 @@ def getFeedDB():
     db.close()
 
     return [uniqueId, rowsJSON]
+
 
 # 피드 댓글을 DB에 등록하기
 @app.route('/feed/post', methods=["POST"])
@@ -162,7 +171,6 @@ def editCommentDB(feedId):
     db.commit()
     db.close()
     return jsonify({'msg': '수정 완료!'})
-
 
 
 if __name__ == '__main__':
